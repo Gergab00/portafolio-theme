d2349f3e9efb3b7e1dd4fe0915ba536b26473c78
Gerardo Gonzalez <gergab00@hotmail.com>
2022-11-05 16:55:35 -0600

    feat: Add front-page.php file

---
+++ front-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 
+ * Description: A page template that provides a key component of WordPress as a CMS
+ * by meeting the need for a carefully crafted introductory page. The front page template
+ * in Twenty Twelve consists of a page content area for adding text, images, video --
+ * anything you'd like -- followed by front-page-only widgets in one or two columns.
+ * 
+ * @package portafolio-theme
+ * 
+ * @version 0.9.0
+ * @since 0.9.0
+ * @author Gerardo Gonzalez
+ * 
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+<main class="<?php echo esc_attr( $container ); ?> bg-dark-shade-blue-magenta" id="front-page">
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+else :
+    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+endif;
+?>
+</main>
+
+<?php
+get_footer();
